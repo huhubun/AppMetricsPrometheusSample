319064c67dd2c4a37b6d125f23e36578aff23406
huhubun <nongzc@outlook.com>
2020-02-02 23:04:25 +0800

    Modify /values route to route attribute

--- src/DotNetFramework/WebAPISample/Controllers/ValuesController.cs
+++ src/DotNetFramework/WebAPISample/Controllers/ValuesController.cs
@@ -1,38 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 
 namespace WebAPISample.Controllers
 {
+    [RoutePrefix("api/values")]
     public class ValuesController : ApiController
     {
         // GET api/values
+        [HttpGet, Route("")]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
-        public string Get(int id)
+        [HttpGet, Route("{id:int}")]
+        public string Get([FromUri]int id)
         {
-            return "value";
+            return "value" + id;
         }
 
         // POST api/values
+        [HttpPost, Route("")]
         public void Post([FromBody]string value)
         {
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        [HttpPut, Route("{id:int}")]
+        public void Put([FromUri]int id, [FromBody]string value)
         {
         }
 
         // DELETE api/values/5
-        public void Delete(int id)
+        [HttpDelete, Route("{id:int}")]
+        public void Delete([FromUri]int id)
         {
         }
     }
