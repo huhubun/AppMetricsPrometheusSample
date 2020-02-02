2c81093f63b919161c1b0bc44113c8d01f754a5d
huhubun <nongzc@outlook.com>
2020-02-02 16:21:46 +0800

    Remove useless files

--- src/DotNetFramework/WebAPISample/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebAPISample.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- src/DotNetFramework/WebAPISample/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebAPISample.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
